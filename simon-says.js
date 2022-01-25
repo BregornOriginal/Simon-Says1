9f658850aa313adf20c2e96060d275b7ff0c2fd9
BregornOriginal <93630700+BregornOriginal@users.noreply.github.com>
2022-01-25 19:43:28 -0300

    Logic for turn player added

--- simon-says.js
+++ simon-says.js
@@ -1,9 +1,9 @@
 let cpu = [];
 let player = [];
 
-window.addEventListener('contextmenu', (event) => {
-    event.preventDefault()
-  })
+
+const colors = ["blue", "red", "yellow", "green"];
+
 document.querySelector("#start-button").onclick = () => {
     startGame();
 };
@@ -21,24 +21,32 @@ reset = () => {
 
 handlerGame = () => {
     document.querySelector("#cpu-turn").style.display = "flex";
-    const $colors = document.querySelectorAll("#button-color");
-    const cpuColor = Math.floor(Math.random() * $colors.length);
-    console.log(cpuColor);
-    cpu.push($colors[cpuColor]);
-    console.log(cpu);
-    lightingCpuColor();
+    const cpuColor = Math.floor(Math.random() * colors.length);
+    cpu.push(cpuColor);
+    // lightingCpuColor();
+    turnPlayer();
 };
 
-lightingCpuColor = () => {
-    for (let i = 0; i < cpu.length; i++) {
-        cpu[i].style.opacity = "1"
-}
-};
+// lightingCpuColor = () => {
+//     for (let i = 0; i < cpu.length; i++) {
+//         cpu[i].style.opacity = "1"
+// }
+// };
 
-let color = setTimeout(lightingCpuColor, 2000);
-
-lightingPlayerColor = () => {
-
+function turnPlayer() {
+    document.querySelectorAll("#button-color").forEach(function (color) {
+        color.onclick = function (event) {
+            if (event.target.getAttribute("name") === "blue") {
+                player.push(0);
+            } else if (event.target.getAttribute("name") === "red") {
+                player.push(1);
+            } else if (event.target.getAttribute("name") === "yellow") {
+                player.push(2);
+            } else if (event.target.getAttribute("name") === "green") {
+                player.push(3);
+            }
+    }
+});
 };
 
 
