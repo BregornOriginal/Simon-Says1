3a08ea2436ebda981e0b2066aaf95917cd682048
BregornOriginal <93630700+BregornOriginal@users.noreply.github.com>
2022-01-25 22:08:39 -0300

    Modify array of cpu and player.
    The arrays of player and cpu now nest the string colors instead numbers, so it's more semantic.
    Added turn of player and cpu.
    Added validation of player secuence.

--- simon-says.js
+++ simon-says.js
@@ -1,60 +1,54 @@
 let cpu = [];
 let player = [];
 
-
 const colors = ["blue", "red", "yellow", "green"];
 
 document.querySelector("#start-button").onclick = () => {
-    startGame();
+  startGame();
 };
 
-startGame = () => {
-    reset();
-    handlerGame();
-}
-
-reset = () => {
-    cpu = [];
-    player = [];
-}
-
-
-handlerGame = () => {
-    document.querySelector("#cpu-turn").style.display = "flex";
-    const cpuColor = Math.floor(Math.random() * colors.length);
-    cpu.push(cpuColor);
-    // lightingCpuColor();
-    turnPlayer();
+const startGame = () => {
+  handlerGame();
 };
 
-// lightingCpuColor = () => {
-//     for (let i = 0; i < cpu.length; i++) {
-//         cpu[i].style.opacity = "1"
-// }
-// };
+const resetGame = () => {
+  cpu = [];
+  player = [];
+};
+
+const turnCpu = () => {
+  cpuColor = colors[Math.floor(Math.random() * colors.length)];
+  cpu.push(cpuColor);
+};
+
+const handlerGame = () => {
+  validateSecuenceOfPlayer(player, cpu);
+  document.querySelector("#cpu-turn").style.display = "flex";
+  turnCpu();
+  turnPlayer();
+};
 
 function turnPlayer() {
-    document.querySelectorAll("#button-color").forEach(function (color) {
-        color.onclick = function (event) {
-            if (event.target.getAttribute("name") === "blue") {
-                player.push(0);
-            } else if (event.target.getAttribute("name") === "red") {
-                player.push(1);
-            } else if (event.target.getAttribute("name") === "yellow") {
-                player.push(2);
-            } else if (event.target.getAttribute("name") === "green") {
-                player.push(3);
-            }
+  document.querySelectorAll("#button-color").forEach(function (color) {
+    color.onclick = function (event) {
+      if (event.target.getAttribute("name") === "blue") {
+        player.push("blue");
+      } else if (event.target.getAttribute("name") === "red") {
+        player.push("red");
+      } else if (event.target.getAttribute("name") === "yellow") {
+        player.push("yellow");
+      } else if (event.target.getAttribute("name") === "green") {
+        player.push("green");
+      }
     }
-});
+  });
 };
 
-
-// playerTurn = () => {
-//     document.querySelectorAll("#button-color").forEach(function (box) {
-//         box.onclick = function (event){
-//             if (event.target.getAttribute(""))
-//         }
-//     }
-
-
+function validateSecuenceOfPlayer(player, cpu) {
+  for (let i = 0; i < player.length; i++) {
+    const element = player[i];
+    if (element !== cpu[i]) {
+      return console.log("lost");
+    }
+  }
+};
